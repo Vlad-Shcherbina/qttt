1b3f817d8df526d6e56da3eb75c14619c088b819
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2012-11-29 21:48:06 +0000

    position printing

--- sample.cc
+++ sample.cc
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "pretty_printing.h"
 
@@ -40,6 +41,10 @@ inline bool is_x(Square s) {
   return get_age(s) % 2 == 0;
 }
 
+inline int get_link(Square s) {
+  return s >> 4;
+}
+
 
 class Position {
 public:
@@ -52,18 +57,42 @@ public:
 
   template<typename stream>
   void print(stream &out) {
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        Square s = field[i*3+j];
-        if (::is_empty(s))
-          out << ".";
-        else if (is_classic(s))
-          out << (is_x(s) ? "x" : "o");
-        else
-          out << "?";
-        out << " ";
+    const string big_x = "**  **  **  **  **";
+    const string big_o = " **** **  ** **** ";
+    string square_contents[9];
+    for (int i = 0; i < 9; i++) {
+      Square s = field[i];
+      if (::is_empty(s)) continue;
+      if (is_classic(s)) {
+        square_contents[i] = is_x(s) ? big_x : big_o;
+        square_contents[i][17] = '1' + get_age(i);
       }
-      out << endl;
+    }
+    for (int age = 0; age < 9; age++) {
+      for (int i = 0; i < 9; i++) {
+        Square s = field[i];
+        if (::is_empty(s)) continue;
+        if (is_classic(s)) continue;
+        if (get_age(s) != age) continue;
+        string mark = (is_x(s) ? "x" : "o") + string(1, '1' + age);
+        square_contents[i] += mark;
+        square_contents[get_link(s)] += mark;
+      }
+    }
+
+    for (int i = 0; i < 3; i++) {
+      if (i > 0)
+        out << "------+------+------" << endl;
+      for (int ii = 0; ii < 3; ii++) {
+        for (int j = 0; j < 3; j++) {
+          string s = square_contents[3*i+j];
+          s += string(18 - s.size(), ' ');
+          if (j > 0)
+            out << "|";
+          out << s.substr(6 * ii, 6);
+        }
+        out << endl;
+      }
     }
   }
 };
@@ -73,7 +102,8 @@ int main() {
   Position p;
   p.field[0] = make_classic(0);
   p.field[1] = make_classic(1);
-  p.field[2] = make_quantum(2, 3);
+  p.field[2] = make_quantum(3, 3);
+  p.field[3] = make_quantum(2, 8);
   p.print(cout);
   cout << "hello, world" << endl;
   return 0;
