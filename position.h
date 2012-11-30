6e90b26bca98651d799a7bc9361ebe267951b5f0
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2012-11-30 21:20:22 +0000

    rudimentary sanity check

--- position.h
+++ position.h
@@ -48,14 +48,56 @@ inline int get_link(Square s) {
 class Position {
 public:
   Square field[9];
+  int age;
 
   Position() {
     for (int i = 0; i < 9; i++)
       field[i] = make_empty();
+    age = 0;
+  }
+
+  bool is_sane() const {
+    // We use asserts instead of return false for better diagnostics.
+    assert(0 <= age && age <= 9);
+
+    for (int i = 0; i < 9; i++) {
+      Square s = field[i];
+      if (is_empty(s)) continue;
+      int age = get_age(s);
+      assert(0 <= age < 9);
+      if (is_classic(s)) continue;
+      int link = get_link(s);
+      assert(0 <= link && link < 9);
+      assert(link != i);
+      int linked = field[link];
+      assert(is_empty(linked) || !is_classic(linked));
+    }
+
+    for (int a = 0; a < 9; a++) {
+      std::cout << a << std::endl;
+      bool found = false;
+      for (int i = 0; i < 9; i++) {
+        Square s = field[i];
+        if (is_empty(s))
+          continue;
+        if (get_age(s) == a) {
+          assert(!found);
+          found = true;
+        }
+      }
+      if (a < age)
+        assert(found);
+      else
+        assert(!found);
+    }
+
+    return true;
   }
 
   template<typename stream>
   void print(stream &out) const {
+    assert(is_sane());
+
     const std::string big_x = "**  **  **  **  **";
     const std::string big_o = " **** **  ** **** ";
     std::string square_contents[9];
--- sample.cc
+++ sample.cc
@@ -16,6 +16,8 @@ int main() {
   p.field[1] = make_classic(1);
   p.field[2] = make_quantum(3, 3);
   p.field[3] = make_quantum(2, 8);
+  p.age = 4;
+
   p.print(cout);
   cout << "hello, world" << endl;
   return 0;
